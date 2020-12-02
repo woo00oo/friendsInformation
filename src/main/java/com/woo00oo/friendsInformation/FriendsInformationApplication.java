0e6b1e853ac4eed1ba17f6b32ccb2dc60e3d2791
woo00oo <96woo94@naver.com>
2020-12-02 11:28:57 +0900

    스프링 부트 환경 설정

---
+++ src/main/java/com/woo00oo/friendsInformation/FriendsInformationApplication.java
@@ -0,0 +1,13 @@
+package com.woo00oo.friendsInformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsInformationApplication.class, args);
+	}
+
+}
