b0f83bf8ec84d9fa06e74b379beaff199770bf73
woo00oo <96woo94@naver.com>
2020-12-10 22:01:48 +0900

    Block 엔터티 생성

---
+++ src/main/java/com/woo00oo/friendsInformation/domain/Block.java
@@ -0,0 +1,25 @@
+package com.woo00oo.friendsInformation.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Block {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String reason;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+}
